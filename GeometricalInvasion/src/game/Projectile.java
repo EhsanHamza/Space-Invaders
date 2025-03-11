c0eb41e9a4f7699ad6206ec303b62d55b87b8dbd
ehamza6 <154003242+EhsanHamza@users.noreply.github.com>
2025-03-11 19:03:54 -0400

    Update Projectile.java

--- GeometricalInvasion/src/game/Projectile.java
+++ GeometricalInvasion/src/game/Projectile.java
@@ -4,6 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.function.Supplier;
 
+/**
+ * The Projectile class represents a projectile in the game.
+ * It moves either upwards or downwards depending on whether it belongs to an enemy or the player.
+ * implements the ProjectileInterface
+ */
 public class Projectile implements ProjectileInterface {
 	private double x, y;
 	private int speed = 15;
@@ -11,6 +16,14 @@ public class Projectile implements ProjectileInterface {
 	private boolean active = true;
 	private boolean enemyProjectile;
 	
+	/**
+     * Creates a projectile object with the specified position and type.
+     *
+     * @param x               The initial x-coordinate of the projectile.
+     * @param y               The initial y-coordinate of the projectile.
+     * @param enemyProjectile true if the projectile belongs to an enemy, 
+     *                        else false
+     */
 	public Projectile(double x, double y, boolean enemyProjectile) {
 		this.x = x;
 		this.y = y;
@@ -22,6 +35,10 @@ public class Projectile implements ProjectileInterface {
 		this.speed = ((Supplier<Integer>) () -> enemyProjectile ? 5 : -10).get();
 	}
 	
+	 /**
+     * Moves the projectile in the specified direction based on its speed.
+     * If the projectile moves outside the screen boundaries, it is inactive.
+     */
 	public void move() {
 		// The projectile supports direction control, depending on the speed value.
 		y += speed;
@@ -33,6 +50,11 @@ public class Projectile implements ProjectileInterface {
 	}
 	
 	// Creates the projectiles itself.
+	/**
+     * Draws projectiles on the screen
+     *
+     * @param brush Draws the graphics
+     */
 	public void paint(Graphics brush) {
 		if (active) {
 			brush.setColor(enemyProjectile ? Color.RED : Color.GREEN);
@@ -41,6 +63,12 @@ public class Projectile implements ProjectileInterface {
 	}
 	
 	// Method for checking if projectiles are colliding with something.
+	/**
+     * Checks whether the projectile is colliding with a given target.
+     *
+     * @param target polygon representing the target.
+     * @return true if the projectile is colliding with the target, else false.
+     */
 	public boolean colliding(Polygon target) {
 		if (target == null) {
 			return false;
@@ -50,11 +78,18 @@ public class Projectile implements ProjectileInterface {
 	}
 	
 	// Returns if projectile is active or not.
+	/**
+	 * checks if the projectile is active
+	 * @return true if it is active, else false.
+	 */
 	public boolean isActive() {
 		return active;
 	}
 	
 	// Sets active to be Inactive, disabling the projectile.
+	/**
+     * Deactivates the projectile
+     */
 	public void setInactive() {
 		this.active = false;
 	}
